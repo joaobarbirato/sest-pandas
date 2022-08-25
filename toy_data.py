3ec66d1c0175eb21f50c892a03577295bc855757
João Barbirato <joaobarbirato@gmail.com>
2022-08-25 00:43:19 -0300

    Create toy_data.py

---
+++ toy_data.py
@@ -0,0 +1,16 @@
+dados = [
+    {"título": "A ameaça fantasma", "ano": 1999, "trilogia": 2, "nota": 6.5, "ranking": 356},
+    {"título": "Ataque dos Clones", "ano": 2002, "trilogia": 2, "nota": 6.6, "ranking": 874},
+    {"título": "A vingança dos sith", "ano": 2005, "trilogia": 2, "nota": 7.6, "ranking": 808},
+    {"título": "Uma nova esperança", "ano": 1977, "trilogia": 1, "nota": 8.6, "ranking": 188},
+    {"título": "Uma nova esperança", "ano": 1977, "trilogia": 1, "nota": 8.6, "ranking": 188},
+    {"título": "O império contra-ataca", "ano": 1980, "trilogia": 1, "nota": 8.7, "ranking": 633},
+    {"título": "O retorno do jedi", "ano": 1983, "trilogia": 1, "nota": 8.3, "ranking": 683},
+    {"título": "Rogue One", "ano": 2016, "trilogia": None, "nota": 7.8, "ranking": 172},
+    {"título": "Han Solo", "ano": '', "trilogia": None, "nota": 6.9, "ranking": 795},
+    {"título": "O despertar da força", "ano": 2015, "trilogia": 3, "nota": 7.8, "ranking": 739},
+    {"título": "Os últimos jedi", "ano": 2017, "trilogia": 3, "nota": 6.9, "ranking": 864},
+    {"título": "A ascensão Skywalker", "ano": 2019, "trilogia": 3, "nota": 6.5, "ranking": 416}
+]
+
+oscars = [0, 0, 0, 6, 1, 0, 0, 0, 0, 0, 0]
